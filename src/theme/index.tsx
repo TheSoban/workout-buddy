363bd7894f1e321c25e5910dfa0f4fe18bb92c12
ryszard-put <ryszard.poznanski0@gmail.com>
2021-10-18 14:20:41 +0200

    Implement theme system

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,7 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import useTheme from "./useTheme";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import {useTheme} from "./useTheme";
+
+const GlobalStyle = createGlobalStyle`
+
+`;
 
 const ThemeWrapper: React.FC = ({children}) => {
-  const obj = useTheme();
-  return <ThemeProvider theme={{}}>{children}</ThemeProvider>
+  const {theme} = useTheme();
+  return <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {children}
+  </ThemeProvider>
 }
+
+export default ThemeWrapper;
--- src/theme/useTheme.tsx
+++ src/theme/useTheme.tsx
@@ -11,20 +11,23 @@ const localStorageSet = (key: string, value: string) => {
   window.localStorage.setItem(key, value);
 }
 
-const useTheme = () => {
+export const useTheme = () => {
   const [theme, setTheme] = useState<ThemeVariant>('light');
 
   const changeTheme = (theme: ThemeVariant) => {
-    
+    localStorageSet('app_theme', JSON.stringify({value: theme}));
+    setTheme(theme);
   }
 
   useEffect(() => {
-
+    const theme = localStorageGet('app_theme');
+    console.log(theme)
+    if(!theme) {
+      localStorageSet('app_theme', JSON.stringify({value: 'light'}));
+      return;
+    }
+    changeTheme(theme.value);
   }, []);
 
-  
-
-  return {}
+  return {theme: themes[theme], changeTheme}
 }
-
-export default useTheme;
