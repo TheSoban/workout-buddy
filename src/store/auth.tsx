f91f9ea40e55dfbca7fc6159f61f57d615c76d42
ryszard-put <ryszard.poznanski0@gmail.com>
2021-10-18 13:01:37 +0200

    Something Something authentication

--- src/store/auth.tsx
+++ src/store/auth.tsx
@@ -1,14 +1,27 @@
 import React, {useContext, createContext, useState, useEffect} from 'react';
-import { useHistory } from 'react-router';
+import API from '../utils/axios';
+
+export type Provider = 'github' | 'facebook' | 'google' | 'local' | null;
 
 export interface AuthStructure {
-  user_id: string;
+  user_id: string | null;
+  provider: Provider;
+  username: string | null;
+  avatar_url: string | null;
   authenticated: boolean;
+}
+
+const defaultUser: AuthStructure = {
+  user_id: null,
+  provider: null,
+  username: null,
+  avatar_url: null,
+  authenticated: false
 }
 
 export interface AuthContextData {
   user: AuthStructure;
-  signin: () => void;
+  signinUsingLocal: () => void;
   signinUsingGithub: () => void;
   signout: () => void;
 }
@@ -18,30 +31,39 @@ export const authContext = createContext<AuthContextData>(null);
 //TODO
 // Remove simple state handling, switch to reducer approach
 const useAuthState = () => {
-  const [user, setUser] = useState<AuthStructure>({user_id: '', authenticated: false});
+  const [user, setUser] = useState<AuthStructure>(defaultUser);
 
   // Auth handlers
-  const signin = () => {
-    setUser(s => ({...s, user_id: '123', authenticated: true}));
+  const signinUsingLocal = () => {
+    setUser((s: AuthStructure) => ({...s, user_id: '123', authenticated: true, provider: 'local'}) as AuthStructure);
   }
 
   const signinUsingGithub = () => {
-    window.location.href = ('https://workout-buddy-api.thesoban.pl/auth/github')
+    window.location.href = ('http://thesoban.pl:4321/auth/github')
   }
 
   const signout = () => {
-    setUser(s => ({...s, user_id: '', authenticated: false}));
+    setUser((s: AuthStructure) => ({...s, user_id: null, authenticated: false}) as AuthStructure);
   }
 
   useEffect(() => {
-    console.log("Auth mount")
+    (async function(){
+      try{
+        const response = await API.get('/getuser');
+        const data = response.data
+        console.log(response)
+        console.log(data)
+      }catch(e){
+        console.log(e);
+      }
+    })();
 
     return () => { console.log("Auth unmount")}
   }, []);
 
   return {
     user,
-    signin,
+    signinUsingLocal,
     signinUsingGithub,
     signout
   }
