cae12a01db714a6758629e239dd91a54b9c4723c
ryszard-put <ryszard.poznanski0@gmail.com>
2021-10-22 10:52:18 +0200

    Add SigninForm

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Redirect } from 'react-router';
+import SigninForm from '../components/SigninForm';
 import { useAuth } from '../store/auth';
 
 const SignIn: React.FC = () => {
   const {user, signinUsingGithub} = useAuth()
   if(user.authenticated) return <Redirect to='panel' />
+
   return <div>
     <h1>Sign In</h1>
-    <button onClick={() => signinUsingGithub()}>Sign in with Github</button>
+    <SigninForm />
   </div>
 }
 
