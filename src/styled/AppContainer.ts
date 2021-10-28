647f331ae6dcf2c74c16326cd3637b4dc1889f47
ryszard-put <ryszard.poznanski0@gmail.com>
2021-10-28 22:50:39 +0200

    Create signin form styled components

---
+++ src/styled/AppContainer.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  background: linear-gradient(135deg, #ff4785, #ffc47d);
+`;
+
+export default AppContainer;
